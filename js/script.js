d079c78bfab9a9e4d3d7af0a082ea2f71144c422
mirtlbu <bubuzoglo@gmail.com>
2014-12-25 12:34:48 +0300

    wip

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
-function createTemplate() {
+function createPartners() {
 	return $('<div/>', {'class': 'list__item'})
 		.append($('<img/>', {'src': 'img/epic-logo.png'}));
-}
+};
+
+function createGallery() {
+	return $('<div/>', {'class': 'gallery__item'})
+		.append($('<img/>', {'src': 'img/rooms.jpg'}))
+		.append($('<span/>').text('Квартира-студия'))
+		.append($('<span/>').text('Однокомнатная квартира-студия, 50м², возле станции метро «Выхино». Цена 10 000 000 рублей.'))
+};
+
+function renderPartners() {
+	for (var i = 0; i < 12; i++) {
+		$('.partners').find('.list').append(createPartners());
+	}
+};
+
+function renderGallery() {
+	for (var i = 0; i < 8; i++) {
+		$('.gallery').append(createGallery());
+	}
+};
 
 $(function() {
-	for (var i = 0; i < 12; i++) {
-		$('.partners').find('.list').append(createTemplate());
-	}
-})
+	renderGallery();
+	renderPartners();
+	$()
+});
+
+
