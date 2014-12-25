3518645b9ccb58a574989dc4a189ad40baff10d8
mirtlbu <bubuzoglo@gmail.com>
2014-12-25 12:59:52 +0300

    slider wip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+var BLOCK = 'fotorama',
+ELEM_WRAP = BLOCK + '__wrap',
+ELEM_ARROW = BLOCK + '__arrow',
+MOD_ARROW_PREV = ELEM_ARROW + '--prev',
+MOD_ARROW_NEXT = ELEM_ARROW + '--next';
+
+function createSwitcher(className, showParam, content) {
+	return $('<div/>', {'class': className, tabindex: 0, role: 'button', 'data-index': showParam})
+		.data('show-param', showParam)
+		.append(content);
+}
+
+$(function() {
+
+	var $fotorama = $('.' + BLOCK)
+		.fotorama({
+			width: 820,
+			height: 560,
+			loop: true,
+			keyboard: true,
+			swipe: true,
+			transitionduration: 1000,
+			spinner: false,
+			arrows: false
+		});
+
+	$fotorama.find('.' + ELEM_WRAP)
+		.append(createSwitcher([ELEM_ARROW, MOD_ARROW_PREV].join(' '), '<'))
+		.append(createSwitcher([ELEM_ARROW, MOD_ARROW_NEXT].join(' '), '>'))
+		.on('click', '.' + ELEM_ARROW, function() {
+			$fotorama.data('fotorama').show($(this).data('show-param'));
+			addActive($fotorama.data('fotorama'));
+		});
+});
+
+
 function createPartners() {
 	return $('<div/>', {'class': 'list__item'})
 		.append($('<img/>', {'src': 'img/epic-logo.png'}));
